975aa6dfabce7e2c5396113a4ea29aeaeed424ad
alvaroormeno <alvaro@ormeno.org>
2022-06-30 16:06:42 -0400

    created first schema for users

---
+++ models/User.js
@@ -0,0 +1,29 @@
+// USER MODEL IS DATA OUR APP IS EXPECTING TO STORE OR RECEIVE WHEN REFERENCING USERS FROM DATABASE
+
+// Import mongoose
+const {Schema, model} = require("mongoose");
+
+const UserSchema = new Schema(
+    //define what kind of data we are expecting
+    {
+        email: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        timestamp: true
+    }
+); 
+
+// export the model
+const User = model("User", UserSchema);
+module.exports = User;
