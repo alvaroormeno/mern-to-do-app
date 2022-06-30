095d9bdc9012604c0102ac24693e512f9418fff1
alvaroormeno <alvaro@ormeno.org>
2022-06-30 19:53:42 -0400

    working on authenticating email and password for log in, both working

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 const User = require('../models/User');
 
-const bcrypt = require('bcryptjs')
+const bycrypt = require('bcryptjs')
 
 const validateRegisterInput = require('../validation/registerValidation')
 
@@ -25,7 +25,7 @@ router.get("/test", (req, res) => {
 router.post("/register", async(req, res) => {
     //try code in here
     try{
-        // destructuring to grab the errors
+        // destructuring to grab the errors and isValid from validateRegisterInput after running register user data
         const {errors, isValid} = validateRegisterInput(req.body);
 
         if(!isValid) {
@@ -43,8 +43,9 @@ router.post("/register", async(req, res) => {
         }
 
         //hash the password
-        const hashedPassword = await bcrypt.hash(req.body.password, 12);
+        const hashedPassword = await bycrypt.hash(req.body.password, 12);
 
+        
         //create new user
         const newUser = new User({
             email: req.body.email,
@@ -54,9 +55,14 @@ router.post("/register", async(req, res) => {
 
         //save the user to the database
         const savedUser = await newUser.save();
+        // After saving it, we need to create a new variable (userToReturn) which wont return the Password for more security...
+        // ...savedUser spreads all the data from the const savedUser into new object, ._doc is needed
+        // to show object as regular json object without mongoose/mongodb extra  meta fields for internal management
+        const userToReturn = {...savedUser._doc};
+        delete userToReturn.password;
 
         // return the new user
-        return res.json(savedUser);
+        return res.json(userToReturn);
         
      //if try doesnt work, catch error
     }catch (err) {
@@ -64,6 +70,36 @@ router.post("/register", async(req, res) => {
         console.log(err);
         // respond error with status(500) which means server error and error message
         res.status(500).send(err.message)
+    }
+})
+
+
+//Route - POST /api/auth/loggin
+//Description - Login user and return access token
+//Access - Public
+router.post("/login", async (req, res) => {
+    try{
+        //STEP 1 - Check for the user email if is already saved on database
+        const user = await User.findOne({
+            email: new RegExp("^" + req.body.email + "$", "i")
+        })
+        // If it is not saved, return error message and stop
+        if(!user) {
+            return res.status(400).json({error: "There was a problem with your login credentials"})
+        }
+        //STEP 2- If the email is saved on Step1, compare password to password saved on database
+        const passwordMatch = await bycrypt.compare(req.body.password, user.password)
+        //  If the password entered doenst match password on database, return error and stop
+        if(!passwordMatch) {
+            return res.status(400).json({error: "There was a problem with your login credentials"})
+        }
+
+        return res.json({passwordMatch: passwordMatch})
+        
+    } catch(err) {
+        console.log(err)
+
+        return res.status(500).send(err.message)
     }
 })
 
