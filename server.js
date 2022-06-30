b350f1fed57b480c57da3ec3f7757e07660a74aa
alvaroormeno <alvaro@ormeno.org>
2022-06-30 15:22:10 -0400

    first commit, server.js created

---
+++ server.js
@@ -0,0 +1,33 @@
+// Allows to access environment variables written in the .env file
+require("dotenv").config();
+
+// Initializes express server
+const express = require("express");
+const app = express();
+
+// Express middleware service - recognizes the incoming Request Object as a JSON Object.
+app.use(express.json());
+// Express middleware service - recognizes the incoming Request Object as Strings or Arrays.
+app.use(express.urlencoded());
+
+// First API ROUTE - Everytime we make a GET request to "/" we then want to return something
+app.get("/", (req,res) => {
+    res.send("Fullstack React Course Express Server");
+})
+
+
+// Second API ROUTE - post route to verify we can get through data we are sending.  
+app.post("/name", (req, res) => {
+    // if req.body.name (body of the request with key: name) is true, return request in json format,
+    // else return request as status 400 with json format message.
+    if(req.body.name) {
+        return res.json({name: req.body.name})
+    } else {
+        return res.status(400).json({error: "No name provided"})
+    }
+})
+
+// SERVER RUNNING - process.env.PORT acceses PORT in .env file
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`)
+})
