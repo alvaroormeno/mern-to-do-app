477e185b497dbec24cf8be6ac10a89695226ffcf
alvaroormeno <alvaro@ormeno.org>
2022-07-03 13:48:12 -0400

    finished api routes to mark complete and incomplete specific todos. Both working and verified on postman

--- routes/todos.js
+++ routes/todos.js
@@ -75,4 +75,99 @@ router.get("/current", requiresAuth, async (req, res) => {
 })
 
 
+//Route - PUT /api/todos/test/:toDoId/complete
+//Description - Mark a todo as complete
+//Access - Private
+router.put("/:toDoId/complete", requiresAuth, async(req, res) => {
+    try {
+        //Grab todo to be able to pass the Id of the todo
+        const toDo = await ToDo.findOne({
+            user: req.user._id,
+            _id: req.params.toDoId,
+        });
+        // error checking 1
+        if(!toDo) {
+            return res.status(404).json({error: 'Could not find a ToDo'})
+        }
+        // error checking 2
+        if(toDo.complete) {
+            return res.status(400).json({error: "ToDo is already complete"})
+        }
+
+        // uodate the todo
+        const updatedToDo = await ToDo.findOneAndUpdate(
+            {
+                user: req.user._id,
+                _id: req.params.toDoId,        
+            },
+            // 2nd paramater we tell what we want to update
+            {
+                complete: true,
+                completedAt: new Date()
+            },
+            {
+                new: true
+            }
+            ); 
+
+            return res.json(updatedToDo);
+
+    } catch(err) {
+        console.log(err);
+        return res.status(500).send(err.message)
+    }
+})
+
+
+//Route - PUT /api/todos/test/:toDoId/incomplete
+//Description - Mark a todo as INCOMPLETE
+//Access - Private
+router.put("/:toDoId/incomplete" , requiresAuth, async (req, res) => {
+    try{
+
+        // STEP 1 - Find specifc ToDo based on the id which is passed on the URL to check for errors
+        const toDo = await ToDo.findOne({
+            user: req.user._id,
+            _id: req.params.toDoId,
+        })
+        // STEP 2 - Find Errors
+        // If ToDo variable is impty/false then return 400 error with message as json
+        if(!toDo) {
+            return res.status(400).json({error: "Could not find ToDo"})
+        }
+        // If ToDo variable is the opposite (!) of a found todo based on the id and complete (therefore incomplete),
+        // then return 400 error with message as json
+        if(!toDo.complete) {
+            return res.status(400).json({error: "ToDo is already incomplete"})
+        }
+        //STEP 3 - FIND AND UPDATE THE TO DO
+        // Note - findOneAndUpdate() accepts 3 params. findOneAndUpdate(A, B, C)
+        // A = query to find, B = what to update, C = option to return new updated data or old before updated data, default is always old before update
+        const updatedToDo = await ToDo.findOneAndUpdate(
+            // (A) Find a specific todo based on the id which is passed on the URL
+            {
+                user: req.user._id,
+                _id: req.params.toDoId,
+            },
+            // (B) Update the complete property value to False, update the completed time to zero therefore Null
+            {
+                complete: false,
+                completedAt: null,
+            },
+            // (C) Option to retun the updated data of ToDo
+            {
+                new: true
+            }
+        );
+        
+        return res.json(updatedToDo)
+
+
+    } catch(err) {
+        console.log(err);
+        return res.status(500).send(err.message);
+    }
+})
+
+
 module.exports = router;
