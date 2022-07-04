cfaf81ac1c38cc921b9ba8f7b27b854834d79e6d
alvaroormeno <alvaro@ormeno.org>
2022-07-04 18:43:23 -0400

    completed global reducer switch statement

--- client/src/context/GlobalContext.js
+++ client/src/context/GlobalContext.js
@@ -3,7 +3,6 @@ import axios from'axios';
 
 
 // INITIAL STATE
-
 const initialState = {
     user: null,
     fetchingUser: true,
@@ -22,11 +21,26 @@ const globalReducer = (state, action) => {
                 fetchingUser: false,
             }
         
-            case "SET_COMPLETE_TODOS":
-                return {
-                    ...state,
-                    completeToDos: action.payload
-                }
+        case "SET_COMPLETE_TODOS":
+            return {
+                ...state,
+                completeToDos: action.payload 
+            }
+        
+        case "SET_INCOMPLETE_TODOS":
+            return {
+                ...state,
+                incompleteToDos: action.payload 
+            }
+
+        case "RESET_USER":
+            return {
+                ...state,
+                user: null,
+                completeToDos: [],
+                incompleteToDos: [],
+                fetchingUser: false,
+            }
 
         default:
             return state;
@@ -36,9 +50,15 @@ const globalReducer = (state, action) => {
 // CREATE THE CONTEXT
 export const GlobalContext = createContext(initialState)
 
-// PROVIDER COMPONENT
+// COMPONENT WHICH WILL NEXT ALL COMPONENTS INSIDE -  PROVIDER COMPONENT
 export const GlobalProvider = (props) => {
     const[state, dispatch] = useReducer(globalReducer, initialState)
+
+
+    
+    useEffect( () => {
+        getCurrentUser()
+    }, [])
 
     // action: get current user - function that will get all the data we need once we think a user is logged in
     const getCurrentUser = async () => {
@@ -47,27 +67,33 @@ export const GlobalProvider = (props) => {
         // request to our backend using axios
         const res = await axios.get("/api/auth/current");
             // if there is a user, we expect data
-        if(res.data) {
-            // request to grab users current todos
-            const toDosRes = await axios.get("/api/todos/current")
-            // if there is data...
-            if(toDosRes.data) {
-                //dispatch 
-                dispatch({type: "SET_USER", payload: res.data});
-                dispatch({type: "SET_COMPLETE_TODOS", payload: toDosRes.data.complete});
-                dispatch({type: "SET_INCOMPLETE_TODOS", payload: toDosRes.data.incomplete})
+            if(res.data) {
+                // request to grab users current todos
+                const toDosRes = await axios.get("/api/todos/current")
+                // if there is data...
+                if(toDosRes.data) {
+                    //dispatch 
+                    dispatch({type: "SET_USER", payload: res.data});
+                    dispatch({type: "SET_COMPLETE_TODOS", payload: toDosRes.data.complete});
+                    dispatch({type: "SET_INCOMPLETE_TODOS", payload: toDosRes.data.incomplete})
+                }
+
+                // if there is no data from the request, reset user
+            } else {
+                dispatch({type: "RESET_USER"})
             }
-        }
 
         } catch(err) {
             console.log(err)
-        }
+            // Also if there is an error with any other request, reset user
+            dispatch({type: "RESET_USER"});
+        };
     }
-
-
 
     const value = {
         ...state,
+        // Exporting this on the value lets us use it in other components to get the current user
+        getCurrentUser,
     }
 
     return (
