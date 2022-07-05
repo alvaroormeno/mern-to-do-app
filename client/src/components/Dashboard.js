d5ee5dd5ddfc6fceec9e295a0d8a8ea123745cb5
alvaroormeno <alvaro@ormeno.org>
2022-07-05 18:41:40 -0400

    created dashboard empty component + import setups

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useGlobalContext } from '../context/GlobalContext'
+import { useNavigate } from 'react-router-dom'
+
+const Dashboard = () => {
+  return (
+    <div>Dashboard</div>
+  )
+}
+
+export default Dashboard
