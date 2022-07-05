6c8ab6aa28ca1dd51fa9e55d67485158af26f9ed
alvaroormeno <alvaro@ormeno.org>
2022-07-05 19:16:12 -0400

    incomplete and complete todos are now rendering on front end dashboard component. mapped through both complete and incomplete data arrays

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom'
 
 const Dashboard = () => {
 
-    const {user} = useGlobalContext();
+    const {user, completeToDos, incompleteToDos} = useGlobalContext();
     const navigate = useNavigate();
 
 
@@ -22,11 +22,26 @@ const Dashboard = () => {
 
   return (
 
-    
+    <div className="dashboard">
+        <div className="todos">
+            {/* Map incompleteToDos Array and display each on front end. Since map needs a key we use the id from the todo */}
+            {incompleteToDos.map((incTodoData) => (
+            <h1 key={incTodoData._id}>{incTodoData.content}</h1>
+            ))}
+        </div>
 
+        {/* We only want to show complete todos if there are any.  */}
+        {completeToDos.length > 0 && (
+            <div className="todos">
+                <h2 className='todos__title'>Completed ToDo's</h2>
+                {completeToDos.map((comTodoData) => (
+                    <h1 key={comTodoData._id}>{comTodoData.content}</h1>
+                ))}
+            </div>
+        )}
+    </div>
 
-    // protect dashboard if there is no user
-    <div>Dashboard</div>
+  
   )
 }
 
