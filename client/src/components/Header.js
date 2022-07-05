3353de5682271318b66e6e30fb110382753d84ba
alvaroormeno <alvaro@ormeno.org>
2022-07-04 20:48:51 -0400

    added onclick to logout button to call logout function from useGlobalContext

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,8 +4,8 @@ import { useGlobalContext } from '../context/GlobalContext';
 
 const Header = () => {
 
-    // Destructure useGlobalContext to be able to grab user
-    const {user} = useGlobalContext();
+    // Destructure useGlobalContext to be able to grab user, logout 
+    const {user, logout} = useGlobalContext();
 
     // Destructure useLocation to be able to use pathname
     const {pathname} = useLocation();
@@ -24,7 +24,8 @@ const Header = () => {
                 page url path, show the register button, if we are not located on "/" show Login button. 
                 Note: Link to register and login shows as button since its using className='btn' */}
                 {user ? (
-                    <button className='btn'>Logout</button>
+                    // onClick={logout} = when we click we are calling logout function in useGlobalContext which logs out the user
+                    <button className='btn' onClick={logout}>Logout</button>
                 ) : pathname === "/" ? (
                     <Link to="/register" className='btn'>
                         Register
