a791fd224eb4d371c0796555af24f10f0de6f15b
alvaroormeno <alvaro@ormeno.org>
2022-07-05 22:28:07 -0400

    add button works, sends the content of new todo to the api but doesnt show unless we refresh page, need to fix it on globalcontext

--- client/src/components/NewToDo.js
+++ client/src/components/NewToDo.js
@@ -1,12 +1,32 @@
-import React from 'react'
+import axios from 'axios'
+import React, {useState} from 'react'
 
 const NewToDo = () => {
 
+    const [content, setContent] = useState("")
 
-    
+    const changeContent = function (e) {
+        setContent(e.target.value)
+    }
+
+    const onSubmit = function(e) {
+        e.preventDefault();
+
+        // STEP 1: axios post call to create new todo api and sends content of input
+        axios.post("/api/todos/new", {content}).then(res => {
+            // then sets back the content state of the input to empty("")
+            setContent("")
+            // action of add to do to add it to our globalcontext
+            //addToDo(res.data)
+        })
+    }
+
   return (
-    <form className="new">
-        <input type="text"  />
+    <form className="new" onSubmit={onSubmit}>
+        <input 
+            type="text"
+            value={content}
+            onChange={changeContent}  />
 
         <button className='btn'>Add</button>
     </form>
